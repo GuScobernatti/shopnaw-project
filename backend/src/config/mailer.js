fc47e9d6e799e224dda2ca3ead1de8a8c221110f
GuScobernatti <fakagula@gmail.com>
2026-01-22 02:10:30 -0300

    Atualização no envio de e-mails P4

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -1,17 +1,15 @@
 const nodemailer = require("nodemailer");
 
-// Configuração de transporte (Exemplo com Gmail)
-// Para produção, use SendGrid, AWS SES, ou Mailgun.
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", // ou host: 'smtp.seuprovedor.com'
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Senha de app do Gmail (não a senha normal)
+    pass: process.env.EMAIL_PASS,
   },
   tls: {
-    rejectUnauthorized: false, // Ajuda a evitar erros de certificado no Render
+    rejectUnauthorized: false,
   },
   connectionTimeout: 10000,
 });
