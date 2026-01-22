6e281fdf708d028e7290f82f758fc54419481232
GuScobernatti <fakagula@gmail.com>
2026-01-21 23:12:21 -0300

    Atualização das configurações do pgPool

--- backend/src/pgPool.js
+++ backend/src/pgPool.js
@@ -1,12 +1,22 @@
 const pg = require("pg");
 const { Pool } = pg;
+const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new Pool({
-  user: "postgres",
-  password: "gustavo26",
-  host: "localhost",
-  port: 5432,
-  database: "shopnaw-ecommerce",
+const poolConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (isProduction) {
+  poolConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const pool = new Pool(poolConfig);
+
+pool.on("error", (err, client) => {
+  console.error("Erro inesperado no cliente do pool", err);
+  process.exit(-1);
 });
 
 module.exports = pool;
